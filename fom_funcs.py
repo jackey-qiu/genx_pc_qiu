fix a bug related to the calculation of structure factor of layered structure (both layered water and sorbate), the ds should not be multiply by the Auc, but it should be replaced by 1

fom_funcs.py
@@ -230,7 +230,16 @@ def R1_weighted(simulations, data):
         if dataset.use])
     return [1.0/denom*(np.sqrt(np.abs(dataset.y)) - np.sqrt(np.abs(sim)))/dataset.error\
         for (dataset, sim) in zip(data,simulations)]
-
+        
+def R1_weighted_2(simulations, data):
+    ''' Crystallographic R-factor (R1)
+    '''
+    denom = np.sum([np.sum(np.sqrt(np.abs(dataset.y))) for dataset in data\
+        if dataset.use])
+    #denom=1
+    return [1.0/denom*(np.sqrt(np.abs(dataset.y)) - np.sqrt(np.abs(sim)))/np.abs(dataset.y)\
+        for (dataset, sim) in zip(data,simulations)]
+        
 def logR1(simulations, data):
     ''' logarithmic crystallographic R-factor (R1)
     '''

models/sxrd_new1.py
@@ -1153,7 +1153,7 @@ class Sample:
         f=self._get_f(np.array(['O']), dinv)[:,0]
         Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
         q=2*np.pi*dinv
-        F_layered_water=f*(Auc*d_w*density_w)*np.exp(-0.5*q**2*u0**2)*np.exp(q*first_layer_height*1.0J)\
+        F_layered_water=f*(Auc*1*density_w)*np.exp(-0.5*q**2*u0**2)*np.exp(q*first_layer_height*1.0J)\
                         /(1-np.exp(-0.5*q**2*ubar**2)*np.exp(q*d_w*1.0J))
         return F_layered_water
         
@@ -1168,7 +1168,7 @@ class Sample:
             f=self._get_f(np.array(['O']), dinv)[:,0]
             Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
             q=2*np.pi*dinv
-            F_layered_water=f*(Auc*d_w*density_w)*np.exp(-0.5*q**2*u0**2)*np.exp(q*(first_layer_height+54.3+height_offset)*1.0J)\
+            F_layered_water=f*(Auc*1*density_w)*np.exp(-0.5*q**2*u0**2)*np.exp(q*(first_layer_height+54.3+height_offset)*1.0J)\
                             /(1-np.exp(-0.5*q**2*ubar**2)*np.exp(q*d_w*1.0J))#54.3=20.1058*(1+1.6) offset height accouting for bulk and surface slab
             return F_layered_water
         else:
@@ -1184,7 +1184,7 @@ class Sample:
         f=self._get_f(np.array([el]), dinv)[:,0]
         Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
         q=2*np.pi*dinv
-        F_layered_sorbate=f*(Auc*d_s*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*first_layer_height_s*1.0J)\
+        F_layered_sorbate=f*(Auc*1*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*first_layer_height_s*1.0J)\
                         /(1-np.exp(-0.5*q**2*ubar_s**2)*np.exp(q*d_s*1.0J))
 
         return F_layered_sorbate
@@ -1205,7 +1205,7 @@ class Sample:
             f=self._get_f(np.array([el]), dinv)[:,0]
             Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
             q=2*np.pi*dinv
-            F_layered_sorbate=f*(Auc*d_s*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*(first_layer_height_s+54.3+height_offset)*1.0J)\
+            F_layered_sorbate=f*(Auc*1*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*(first_layer_height_s+54.3+height_offset)*1.0J)\
                             /(1-np.exp(-oc_bar)*np.exp(-0.5*q**2*ubar_s**2)*np.exp(q*d_s*1.0J))
             return F_layered_sorbate
         else:
@@ -1220,7 +1220,7 @@ class Sample:
         f=self._get_f(np.array([el]), dinv)[:,0]+(f1f2[:,0]+1.0J*f1f2[0:,1])#atomic form factor corrected by the f1f2 correction items
         Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
         q=2*np.pi*dinv
-        F_layered_sorbate=f*(Auc*d_s*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*first_layer_height_s*1.0J)\
+        F_layered_sorbate=f*(Auc*1*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*first_layer_height_s*1.0J)\
                         /(1-np.exp(-0.5*q**2*ubar_s**2)*np.exp(q*d_s*1.0J))
         return F_layered_sorbate
         
@@ -1244,7 +1244,7 @@ class Sample:
             f=self._get_f(np.array([el]), dinv)[:,0]+(f1f2[:,0]+1.0J*f1f2[:,1])#atomic form factor corrected by the f1f2 correction items
             Auc=self.unit_cell.a*self.unit_cell.b*np.sin(self.unit_cell.gamma)
             q=2*np.pi*dinv
-            F_layered_sorbate=f*(Auc*d_s*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*(first_layer_height_s+54.3+height_offset)*1.0J)\
+            F_layered_sorbate=f*(Auc*1*density_s)*np.exp(-0.5*q**2*u0_s**2)*np.exp(q*(first_layer_height_s+54.3+height_offset)*1.0J)\
                             /(1-np.exp(-oc_bar)*np.exp(-0.5*q**2*ubar_s**2)*np.exp(q*d_s*1.0J))
             return F_layered_sorbate
         else: